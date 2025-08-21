a185e5e8176f701b9a6a29c4264b6dfb8d740544
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-08-21 22:01:19 +0900

    feat: Add analytics and speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics as AnalyticsVercel } from '@vercel/analytics/next'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -106,6 +108,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Footer />
           </SectionContainer>
         </ThemeProviders>
+        <SpeedInsights />
+        <AnalyticsVercel />
       </body>
     </html>
   )
