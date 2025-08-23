696ffcc2be386912fbab1c02f84cd0dca1a4a603
LeHoangTuanbk <43330556+LeHoangTuanbk@users.noreply.github.com>
2025-08-23 09:57:10 +0900

    fix(kbar): dark mode css

--- features/theme-switching/lib/use-theme.ts
+++ features/theme-switching/lib/use-theme.ts
@@ -1,16 +1,9 @@
 'use client'
 
 import { useTheme as useNextTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
 
 export const useTheme = () => {
-  const [mounted, setMounted] = useState(false)
   const { theme, setTheme, resolvedTheme } = useNextTheme()
-
-  // useEffect only runs on the client, so now we can safely show the UI
-  useEffect(() => {
-    setMounted(true)
-  }, [])
 
   const toggleTheme = () => {
     setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark')
@@ -21,6 +14,5 @@ export const useTheme = () => {
     resolvedTheme,
     setTheme,
     toggleTheme,
-    mounted,
   }
 }
